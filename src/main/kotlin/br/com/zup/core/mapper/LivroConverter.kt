76d8efa788414bc25cf58ed870aa6ae8df4083da
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-07-19 09:56:10 -0300

    Criar LivroConverter

---
+++ src/main/kotlin/br/com/zup/core/mapper/LivroConverter.kt
@@ -0,0 +1,32 @@
+package br.com.zup.core.mapper
+
+import br.com.zup.core.model.Livro
+import br.com.zup.database.LivroEntity
+
+
+class LivroConverter {
+    companion object {
+        fun toTransformlivroEntityToLivro(livroEntity: LivroEntity) =
+            Livro(
+                livroEntity.id,
+                livroEntity.nome,
+                livroEntity.nomeAutor,
+                livroEntity.anoDePublicacao
+            )
+        fun livroToLivroEntity(livro: Livro) =
+            LivroEntity(
+                livro.id,
+                livro.nome,
+                livro.nomeAutor,
+                livro.anoDePublicacao
+            )
+
+        fun livroEntitytoLivro(livroEntity: LivroEntity) =
+            Livro(
+                livroEntity.id,
+                livroEntity.nome,
+                livroEntity.nomeAutor,
+                livroEntity.anoDePublicacao
+            )
+    }
+}
