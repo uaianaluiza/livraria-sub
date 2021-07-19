508f5aec40c42992abbe5558e3605c212c8c82b2
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-07-19 09:42:30 -0300

    Criar LivroRepositiryPort

--- src/main/kotlin/br/com/zup/core/port/LivroRepositoryPort.kt
+++ src/main/kotlin/br/com/zup/core/port/LivroRepositoryPort.kt
@@ -1,4 +1,15 @@
 package br.com.zup.core.port
 
+import br.com.zup.core.model.Livro
+import br.com.zup.database.LivroEntity
+import java.util.*
+import javax.inject.Singleton
+
+@Singleton
 interface LivroRepositoryPort {
+    fun create(livro:Livro): LivroEntity
+    fun getAll(): List<Livro>
+    fun getById(id: UUID): Livro?
+    fun delete (id:UUID)
+    fun update(id:UUID,livro: Livro):LivroEntity
 }
