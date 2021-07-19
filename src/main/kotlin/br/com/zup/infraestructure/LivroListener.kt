84ec6fae56d4fe9ba743cb5abbbc2f326704460f
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-07-19 18:55:42 -0300

    Criar LivroListener

--- src/main/kotlin/br/com/zup/infraestructure/LivroListener.kt
+++ src/main/kotlin/br/com/zup/infraestructure/LivroListener.kt
@@ -1,4 +1,19 @@
 package br.com.zup.infraestructure
 
-class LivroListener {
+import br.com.zup.core.model.Livro
+import br.com.zup.core.service.LivroServiceImpl
+import io.micronaut.nats.annotation.NatsListener
+import io.micronaut.nats.annotation.Subject
+import org.slf4j.LoggerFactory
+
+@NatsListener
+class LivroListener(private val livroServiceImpl: LivroServiceImpl) {
+    private val logger = LoggerFactory.getLogger(LivroListener::class.java)
+
+    @Subject("livros")
+    fun receive(livro:Livro){
+        livroServiceImpl.create(livro)
+        logger.info("message received")
+        println("Got livro - $livro")
+    }
 }
